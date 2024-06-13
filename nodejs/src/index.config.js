b4842ab3d2444fbb32ccc5052d387feef0ba41c4
zcyxa <96159802+zcyxa@users.noreply.github.com>
2024-06-13 16:48:35 +0800

    Update index.config.js

--- nodejs/src/index.config.js
+++ nodejs/src/index.config.js
@@ -95,6 +95,17 @@ appys: {
       search: true
     }]
   },
+avlive: {
+        url: [
+           { name: "性视界", "url": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/lndsqhj/zblive/main/YG.txt" , index: "true" }, 
+           { name: "玩偶", "url": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/lndsqhj/zblive/main/hongkongdoll.txt" , index: "true" }, 
+           { name: "传媒仓库", "url": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/lndsqhj/zblive/main/hsck.txt" , index: "true" },       
+           { name: "三级", "url":"https://mirror.ghproxy.com/https://raw.githubusercontent.com/lndsqhj/zblive/main/idol.txt", index: "true" },
+           { name: "刘玥", "url":"https://mirror.ghproxy.com/https://raw.githubusercontent.com/lndsqhj/zblive/main/juneliu.txt", index: "true" }, 
+           { name: "小姐姐", "url":"https://mirror.ghproxy.com/https://raw.githubusercontent.com/lndsqhj/zblive/main/madou.txt", index: "true" },
+           { name: "麻豆", "url": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/lndsqhj/zblive/main/monlingwu.txt", index: "true" }
+        ]
+      },
 avm3u8: {
         md1m3u8: [{
             name: "麻豆①",
