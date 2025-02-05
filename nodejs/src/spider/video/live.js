1dccccffb2f6bc19041e4c695144aa5160869c0b
zcyxa <96159802+zcyxa@users.noreply.github.com>
2025-02-05 15:11:47 +0800

    Update live.js

--- nodejs/src/spider/video/live.js
+++ nodejs/src/spider/video/live.js
@@ -423,7 +423,7 @@ async function test(inReq, outResp) {
 export default {
     meta: {
         key: 'live',
-        name: '🟢 直播频道',
+        name: '直播频道',
         type: 3,
     },
     api: async (fastify) => {
